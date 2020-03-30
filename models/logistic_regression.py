e571fec943f0f5a2796d2c721a4f594290243dea
Marius Bild <mbild@phys.ethz.ch>
2020-03-30 15:25:38 +0200

    Updated version: -fixed the extraction of case number by region -fitting still gives weired values

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -9,17 +9,25 @@ from scipy.stats import linregress
 
 
 class Predictor: 
-
+    """
+    General class for fitting and extrapolating harmonized data.
+    """
     def __init__(self, data_group, model): 
         self.data = data_group
         self.model = model
         self.predictor = None
 
     def fit_data(self, value_type = 'positive_total'): 
-        
+        """
+        fit self.data of a given value_type to self.model 
+        """
         group_infected = self.data.groupby('value_type').get_group(value_type)
+        group_infected = group_infected.\
+                            loc[~pd.isna(group_infected.region_name) \
+                               & pd.isna(group_infected.area_name)]
+        self.data_fit = group_infected
         
-        dates = group_infected['time_report'].unique()
+        dates = group_infected['time_report']
         datetime_dates = pd.to_datetime(dates)
 
         self.start_time = min(datetime_dates)
@@ -27,23 +35,30 @@ class Predictor:
         self.value_type = value_type
 
         times = np.array([])
-        case_numbers = np.array([])
+        case_numbers = group_infected['value'].astype('float64').iloc[:]
+        case_numbers = self.clean_value_series(case_numbers).to_numpy()
 
         for date in dates: 
-            values_date = group_infected.loc[group_infected['time_report']== date]['value']
-            values_date = self.clean_value_series(values_date)
-            case_numbers = np.append(case_numbers, values_date.sum()) 
-            
             delta = (pd.to_datetime(date) - self.start_time).days
-            times = np.append(times, int(delta))
-
+            times = np.append(times, float(delta))
+        
         self.fit_range = int( (max(datetime_dates) - min(datetime_dates)).days )
 
         init_values = self.model.get_init_values(times, case_numbers)
-        self.predictor = self.fit_data_to_model(times, case_numbers, self.model.function, init_values)
-        print(self.predictor)
-
+        self.predictor, self.fit_errors = self.fit_data_to_model(times, case_numbers, \
+                                                                 self.model.function, \
+                                                                 init_values)
+       
     def extrapolate(self, timespan, filepath = None): 
+        """
+        extrapolate data over a given timespan 
+        Input:
+            - timespan --> int. Extrapolation over <timespan> days starting
+                                from the last day in self.data_fit 
+            - filepath --> str. If not none, write extrapolated data into seperate file
+        Output: 
+            - data_extrapolated --> np.ndarray: extrapolated values 
+        """
         if self.predictor is None:
             raise Exception('no fitted model available!')
         days = [d for d in range(timespan)]
@@ -53,7 +68,7 @@ class Predictor:
         self.data_extrapolated = self.append_new_data(values, datetimes) 
         if not filepath is None: 
             self.write_csv(self.data_extrapolated, filepath)
-            return
+            return None
 
         return self.data_extrapolated
 
@@ -75,16 +90,20 @@ class Predictor:
         """
         Return the extrapolated values from 
         """
-        extrapolated_values = self.predictor(times_new)
-        time_deltas = [ timedelta(days = d) for d in times_new ]
+        if isinstance(times_new, list):
+            times_new = np.array(times_new)
+
+        extrapolated_values = self.predictor(times_new).astype(int)
+        time_deltas = [ timedelta(days = int(d)) for d in times_new ]
         time_stamps_new = [ self.end_time + delta for delta in time_deltas ]
 
         return time_stamps_new, extrapolated_values
 
     def append_new_data(self, new_values, new_timestamps): 
-
-        data_copy = self.data.groupby('value_type').get_group(self.value_type)
-        data_copy = data_copy[ -self.extrapolation_timespan: ]
+        """
+        add extrapolated data to the existing frame
+        """
+        data_copy = self.data_fit.iloc[-self.extrapolation_timespan:].copy()
         data_copy['values'] = new_values
         data_copy['time_report'] = new_timestamps  
         data_extrapolated = pd.concat([self.data, data_copy], ignore_index= True)     
@@ -92,6 +111,10 @@ class Predictor:
         return data_extrapolated
 
     def clean_value_series(self, values): 
+        """
+        screen the pandas series  <values> for non numericals
+        This can be improved. Quick fix below 
+        """
         for ind, val in enumerate(values): 
             try: 
                 val = int(val)
@@ -101,20 +124,27 @@ class Predictor:
         return values
 
 class LogisticRegression: 
-
+    """
+    Class for logistic regression. 
+    Every model class should have a function method and get_init_values
+    method, such that it can be used by the <Predictor> class for fitting 
+    and extrapolating
+    """
     def __init__(self):
         pass 
 
     def function(self, x, b, a):
-
         return 1./(1. + a * np.exp(-(b*x)))
 
     def get_init_values(self, x, y): 
-
-        y_tr = np.log(1./y - 1)
-        res = linregress(x,y)
-
-        return res[0], np.exp(res[1])
+        
+        y_tr = np.log(1./y - 1) 
+        res = linregress(x, y_tr)
+        alpha = np.exp(res[1])
+        beta = res[0]
+        alpha= 0.5
+        beta = 2
+        return beta, alpha
 
 
 
